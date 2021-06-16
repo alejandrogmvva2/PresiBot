dc5b1cddd8e9f3e990d2efc14378db1eb4d5beaf
alejandrogmvva2 <42662368+alejandrogmvva2@users.noreply.github.com>
2021-06-16 17:45:26 +0200

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -1,17 +1,16 @@
 const nsfwmenu = (prefix, pushname) => {
-    return `*Comandos para ver pornito 🔞*
-*NSFW ✅*
+    return `*Comandos para pajeros 🔞*
 
 - ${prefix}nsfwbobs
 - ${prefix}nsfwsidebobs
 - ${prefix}nsfwahegao
 - ${prefix}nsfwfeets
 
-_El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
+_Presi necesita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
 
-Algunas funciones fueron eliminas por errores en el servidor de la India
+Hay algunos errores, lo siento.
 
-by shanduy`
+by @illo.presi`
 
 }
 
