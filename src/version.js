57371d15cc9b76c47470471f1fdb41c0eda015ea
alejandrogmvva2 <42662368+alejandrogmvva2@users.noreply.github.com>
2021-06-16 17:48:02 +0200

    Update version.js

--- src/version.js
+++ src/version.js
@@ -1,32 +1,10 @@
 const version = (prefix, pushname) => {
     return `
-*ShanBot 🤖*
+*PresiBot*
 
-*Actualizado:* 15 de junio de 2021
-*Versión actual:* 2.4
-*Ofrecida por:* shanduy™
-
-
-*NOVEDADES*
-
-Actualizacion de la version @adiwajshing/baileys a la mas estable
-
-Nuevo motor WAConnection :)
-
-
-*INFORME*
-
-Si no funciona el comando *play checa el blog del bot y mira la version que poses. 
-Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento.
-Cambiare la apikey pasando un mes o si veo muchos atercados o robos.
-Si vez a alguien que robo mi bot comunicate conmigo.
-
-
-*BLOG DE SHANBOT*
-
-https://github.com/shanduy/ShanBot
-
-by shanduy
+*Actualizado:* 16/06/2021
+*Versión actual:* 2.5
+*Ofrecida por:* Presi
 `
 
 }
